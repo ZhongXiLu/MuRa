55398b95f56386e24b15f8fa274fc13fab391bfe
ZhongXiLu <zhongxilu@hotmail.com>
2020-05-15 15:41:14 +0200

    Updated 'finding optimal weights' script again

--- study/main.py
+++ study/main.py
@@ -4,6 +4,8 @@ import sys
 
 from scipy.optimize import basinhopping, dual_annealing, differential_evolution
 
+N_FEATURES = 1
+
 
 class BasicMutant:
 
@@ -25,6 +27,8 @@ def mutants_from_CSV(exportedFile):
         lines = f.readlines()
 
         parameters = [parameter for parameter in lines[0].split(',')]
+        global N_FEATURES
+        N_FEATURES = len(parameters)
 
         bug_related_mutants = []
         for line in lines[1:]:
@@ -116,25 +120,25 @@ for bugReport in [f for f in os.listdir(sys.argv[1]) if not f.__contains__("bug"
     mutants[bugReport[:-4]] = mutants_from_CSV(sys.argv[1] + "/" + bugReport)
 
 print("\nbasinhopping (Powell)")
-result = basinhopping(evaluate_rankings, [0.5] * 13, minimizer_kwargs=dict(method="Powell"), T=5.0,
+result = basinhopping(evaluate_rankings, [0.5] * N_FEATURES, minimizer_kwargs=dict(method="Powell"), T=5.0,
                       stepsize=10.0, interval=20)
 print_results(result.x)
 
 print("\ndual_annealing (Powell)")
 result = dual_annealing(evaluate_rankings, local_search_options=dict(method="Powell"),
-                        bounds=[(-1, 1)] * 13)
+                        bounds=[(-1, 1)] * N_FEATURES)
 print_results(result.x)
 
 print("\nbasinhopping (COBYLA)")
-result = basinhopping(evaluate_rankings, [0.5] * 13, minimizer_kwargs=dict(method="COBYLA"),
+result = basinhopping(evaluate_rankings, [0.5] * N_FEATURES, minimizer_kwargs=dict(method="COBYLA"),
                       T=5.0, stepsize=10.0, interval=20)
 print_results(result.x)
 
 print("\ndual_annealing (COBYLA)")
 result = dual_annealing(evaluate_rankings, local_search_options=dict(method="COBYLA"),
-                        bounds=[(-1, 1)] * 13)
+                        bounds=[(-1, 1)] * N_FEATURES)
 print_results(result.x)
 
 print("\ndifferential_evolution")
-result = differential_evolution(evaluate_rankings, bounds=[(-1, 1)] * 13, workers=1, maxiter=200)
+result = differential_evolution(evaluate_rankings, bounds=[(-1, 1)] * N_FEATURES, workers=1, maxiter=200)
 print_results(result.x)
