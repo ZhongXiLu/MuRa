aef08039f11c4ff971ce97d0fa76e7c10673a6f1
ZhongXiLu <zhongxilu@hotmail.com>
2020-05-26 17:46:15 +0200

    Updated learning script: also output the best model + formatted output

--- study/main.py
+++ study/main.py
@@ -4,7 +4,7 @@ import sys
 
 from scipy.optimize import basinhopping, dual_annealing, differential_evolution
 
-N_FEATURES = 1
+FEATURES = []
 
 
 class BasicMutant:
@@ -14,10 +14,16 @@ class BasicMutant:
         self.is_bug_related = is_bug_related
 
 
-def print_results(results):
-    print(results, evaluate_rankings(results))
-    for bugReport in sorted([f for f in os.listdir(sys.argv[1]) if not f.__contains__("bug")]):
-        print("\t", bugReport[:-4], "\t", evaluate_ranking(mutants[bugReport[:-4]], results))
+def print_results(method_name, results, verbose=False):
+    ranking_score = evaluate_rankings(results)
+    print("{}: {}".format(method_name, ranking_score))
+    if verbose:
+        for i in range(len(results)):
+            print("{}: {}".format(FEATURES[i], results[i]))
+        print()
+        for bugReport in sorted([f for f in os.listdir(sys.argv[1]) if not f.__contains__("bug")]):
+            print("\t{}: {}".format(bugReport[:-4], evaluate_ranking(mutants[bugReport[:-4]], results)))
+    return ranking_score
 
 
 def mutants_from_CSV(exportedFile):
@@ -27,14 +33,17 @@ def mutants_from_CSV(exportedFile):
         lines = f.readlines()
 
         parameters = [parameter for parameter in lines[0].split(',')]
-        global N_FEATURES
-        N_FEATURES = len(parameters)
+        if not parameters[-1].strip():  # remove trailing comma
+            del parameters[-1]
+        global FEATURES
+        FEATURES = parameters
 
         bug_related_mutants = []
         for line in lines[1:]:
             coeffs = []
             for i, value in enumerate(line.split(",")):
-                coeffs.append((parameters[i], float(value)))
+                if value.strip():
+                    coeffs.append((parameters[i], float(value)))
             bug_related_mutants.append(BasicMutant(coeffs, True))
 
         with open(exportedFile) as f2:
@@ -42,7 +51,8 @@ def mutants_from_CSV(exportedFile):
             for line in f2.readlines()[1:]:
                 coeffs = []
                 for i, value in enumerate(line.split(",")):
-                    coeffs.append((parameters[i], float(value)))
+                    if value.strip():
+                        coeffs.append((parameters[i], float(value)))
 
                 bug_related = False
                 for m, bug_related_mutant in enumerate(bug_related_mutants):
@@ -119,16 +129,28 @@ mutants = {}
 for bugReport in [f for f in os.listdir(sys.argv[1]) if not f.__contains__("bug")]:
     mutants[bugReport[:-4]] = mutants_from_CSV(sys.argv[1] + "/" + bugReport)
 
-print("\nbasinhopping")
-result = basinhopping(evaluate_rankings, [0.5] * N_FEATURES, minimizer_kwargs=dict(method="COBYLA"),
+best_score = 1.0
+best_model = None
+
+result = basinhopping(evaluate_rankings, [0.5] * len(FEATURES), minimizer_kwargs=dict(method="COBYLA"),
                       T=5.0, stepsize=10.0, interval=20)
-print_results(result.x)
+score = print_results("basinhopping", result.x)
+if score < best_score:
+    best_score = score
+    best_model = result.x
 
-print("\ndual_annealing")
 result = dual_annealing(evaluate_rankings, local_search_options=dict(method="COBYLA"),
-                        bounds=[(-1, 1)] * N_FEATURES)
-print_results(result.x)
+                        bounds=[(-1, 1)] * len(FEATURES))
+score = print_results("dual_annealing", result.x)
+if score < best_score:
+    best_score = score
+    best_model = result.x
 
-print("\ndifferential_evolution")
-result = differential_evolution(evaluate_rankings, bounds=[(-1, 1)] * N_FEATURES, workers=-1, maxiter=200)
-print_results(result.x)
+result = differential_evolution(evaluate_rankings, bounds=[(-1, 1)] * len(FEATURES), workers=-1, maxiter=200)
+score = print_results("differential_evolution", result.x)
+if score < best_score:
+    best_score = score
+    best_model = result.x
+
+print()
+print_results("BEST MODEL", best_model, verbose=True)
